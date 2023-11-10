83bd0a8cc578582bab08181eebfed4bbb7b1b4b0
Susana Solis Flores <Suzy.flores@icloud.com>
2023-11-09 19:30:37 -0500

    intro to context, call, bind, apply

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-// Your code here
-function createEmployeeRecord(data){
-    const [firstName, familyName, title, payPerHour] = data;
-    
-    const payroll = {
-        firstName: firstName,
-        familyName: familyName,
-        title: title,
-        payPerHour: parseInt(payPerHour, 10),
-        timeInEvents: [],
-        timeOutEvents: []
-    }
-    return payroll;
+function createEmployeeRecord() {
+  return `${this.firstName}, ${this.familyName}, ${this.title}, ${this.payPerHour}`;
 }
+
+const payroll = {
+  firstName: "John",
+  familyName: "Doe",
+  title: "Software Engineer",
+  payPerHour: 20,
+  timeInEvents: [],
+  timeOutEvents: []
+};
+
+const boundCreateEmployeeRecord = createEmployeeRecord.bind(payroll);
+console.log(boundCreateEmployeeRecord())
+
 function createEmployeeRecords(employeeData) {
     return employeeData.map((data) => {
       const [firstName, familyName, title, payPerHour] = data;
@@ -23,8 +25,15 @@ function createEmployeeRecords(employeeData) {
         timeInEvents: [],
         timeOutEvents: [],
     };
-})
+});
 }
+const employeeData = [
+  ["John", "Doe", "Software Engineer", "20"],
+  ["Jane", "Smith", "Product Manager", "25"],
+];
+const boundMap = createEmployeeRecords.bind(null, employeeData);
+console.log(boundMap());
+
 function createTimeInEvent(employee, dateTimeString) {
     const [date, time] = dateTimeString.split(" ");
     const [year, month, day] = date.split("-");
@@ -41,6 +50,11 @@ function createTimeInEvent(employee, dateTimeString) {
     employee.timeInEvents.push(timeInEventObj);
     return employee;
   }
+  const employee = {name: "John Doe"};
+  const dateTimeString = "2022-01-01 09:00";
+  const boundfunction = createTimeInEvent.bind(employee);
+  boundfunction(dateTimeString);
+
   function createTimeOutEvent(employee, dateTimeString) {
     const [date, time] = dateTimeString.split(" ");
     const [year, month, day] = date.split("-");
@@ -50,12 +64,13 @@ function createTimeInEvent(employee, dateTimeString) {
       date: date,
       hour: parseInt(time),
     };
-    if (!employee.timeOutEvents) {
-      employee.timeOutEvents = [];
+    if (!this.timeOutEvents) {
+      this.timeOutEvents = [];
     }
-    employee.timeOutEvents.push(timeOutObj);
-    return employee;
+    this.timeOutEvents.push(timeOutObj);
+    return this;
   }
+ 
   function hoursWorkedOnDate(record, dateMatched) {    
     const timeInEvent = record.timeInEvents.find(event => event.date === dateMatched);
     const timeOutEvent = record.timeOutEvents.find(event => event.date === dateMatched);
@@ -63,42 +78,40 @@ function createTimeInEvent(employee, dateTimeString) {
     const elapsedHours = elapsedMilliseconds / 100;
     return elapsedHours;
   }
+  const boundFunction = hoursWorkedOnDate(record);
+  const dateMatched = "2022-01-01";
+  boundFunction(dateMatched)
+
   function wagesEarnedOnDate(record, dateMatched) {
     const [date] = dateMatched.split(" ");
-    const hoursWorked = hoursWorkedOnDate(record, dateMatched);
+    const hoursWorked = hoursWorkedOnDate.bind(this)(record, dateMatched);
     const hourlyRate = parseInt(record.payPerHour, 10);
     const wagesEarned = hoursWorked * hourlyRate;
     return wagesEarned;
   }
+
  function allWagesFor(record) {
     const dates = record.timeOutEvents.map(function(object){
         return object.date
     });
     let totalWages = 0;
-//   console.log(dates)
     dates.forEach(function(date) {
-        const wages = wagesEarnedOnDate(record, date);
-        // console.log(wages)
-        // console.log(wages)
+        const wages = wagesEarnedOnDate.bind(this)(record, date);
         totalWages += wages;
-        // console.log(totalWages)
     });
     return totalWages;
   }
+
 function calculatePayroll(record){
     console.log(record)
-//iterate through the array of employee records and for each 
-//employee, calculate the pay owed for each date using 
-//wagesEarnedOnDate function.
-//Then, accumulate the total pay owed for all dates worked 
-//by each employee
+
 let totalPayOwed = 0;
 
 record.forEach(employee => {
 
 employee.timeInEvents.forEach(timeInEvent => {
 const date = timeInEvent.date;
-const payOwed = wagesEarnedOnDate(employee, date);
+const payOwed = wagesEarnedOnDate.call(this, employee, date);
 
 totalPayOwed += payOwed;
 })
@@ -106,9 +119,6 @@ totalPayOwed += payOwed;
 return totalPayOwed
  }
 
-  //parameter is an array of employee records
-    //return total sum of pay as a number
-    //using wagesEarnedOnDate, accumulate value of all dates worked by employee in the record- as an integer.
 
 
     
